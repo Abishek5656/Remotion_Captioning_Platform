af209ecf88637d76e35ac3cb32e5a591eb24005e
abishekvarmapenmetsa <abishekvarma@newowl.co>
2025-12-17 18:36:30 +0530

    whisper

--- backend/src/utils/whisper.js
+++ backend/src/utils/whisper.js
@@ -1,9 +1,20 @@
 import { spawn } from "child_process";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pythonScript = path.resolve(__dirname, "../../whisper_runner.py");
+
+console.log("pythonScript---->",pythonScript)
 
 const PYTHON_PATH =
   "C:/Users/shilp/AppData/Local/Programs/Python/Python310/python.exe";
+
+  const PYTHON_CMD = process.env.PYTHON_CMD || "python3";
 
 export function runWhisper(audioPath, outputDir) {
   return new Promise((resolve, reject) => {
@@ -12,7 +23,12 @@ export function runWhisper(audioPath, outputDir) {
     const outputFile = path.join(outputDir, "transcript.json");
     const pythonScript = path.resolve("whisper_runner.py");
 
-    const child = spawn(PYTHON_PATH, [
+    const PYTHON_SCRIPT = path.resolve(
+  __dirname,
+  "../../whisper_runner.py"
+);
+
+    const child = spawn(PYTHON_CMD, [
       pythonScript,
       audioPath,
       outputFile,
